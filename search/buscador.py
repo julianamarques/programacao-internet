d6848d9689e48a83f89f5d61511443f0323fde2f
julianamarques <julianabmarques@live.com>
2019-04-16 00:02:30 -0300

    Update buscador.py

--- search/buscador.py
+++ search/buscador.py
@@ -5,61 +5,42 @@ from bs4 import BeautifulSoup
 class Search:
     def __init__(self):
         pass
-    
-    def recursive_search(self, url, deep, keyword, search):
-        links = []
-        links_list = []
 
-        if deep == 0:
-            return search(url, keyword)
-
-        response = requests.get(url)
-
-        if response is not None:
-            if response.status_code == 200:
-                soup = BeautifulSoup(response.text, "html.parser")
-                links = soup.find_all("a")
-
-                for l in a:
-                    #try:
-                    if (str(l['href']).startswith("http")):
-                        if l["href"] not in links_list:
-                            links_list.append(l["href"])
-                            search(l["href"], keyword)
-                    #except:
-                    #    pass
-
-                if deep > 0:
-                    extract_links(links, deep - 1, keyword)
-
-                return links
-
-    def extract_links(self, links, deep, keyword, recursive_search):
-        for l in links:
-            links.append(recursive_search(l, deep, keyword))
-
-        return links
-
-    def search(self, url, keyword):
+    def search(self, url, keyword, deep = 0):
         words_list = []
         response = requests.get(url)
-
-        text = BeautifulSoup(response.text, "html.parser").text
-
-        words = re.findall('\w*.{0,10}' + str(keyword) + '.{0,10}\w*', text, re.IGNORECASE)
+        content = BeautifulSoup(response.text, "html.parser").text
+        words = re.findall(  '\w*.{0,10}' + str(keyword) + '.{0,10}\w*' , content, re.IGNORECASE)
+        links = self.extract_links(response)
 
         for w in words:
             words_list.append(w)
 
+        if len(words_list) == 0:
+            return "Nenhum resultado Encontrado"
+
+        if deep > 0:
+            for l in links:
+                self.search(l, keyword, deep - 1)
+        
         return words_list
 
+    def extract_links(self, response):
+        soup = BeautifulSoup(response.text, 'html.parser')
+        links = soup.find_all('a')
+        links_list = []
+
+        for l in links:
+            links_list.append(l["href"])
+
+        return links
 
 def main():
     url = input("URL: ")
-    keyword = input("Keyword : ")
+    keyword = input("Palavra : ")
     deep = int(input("Profundidade: "))
     search = Search()
-    print(search.recursive_search(url, deep, keyword, search.search(url, deep)))
-    
-if __name__ == '__main__':
+    print(search.search(url, keyword, deep))
+
+if __name__ == "__main__":
     main()
